528cfd53890a9baacb7aaf57787969cc24399f72
Matthew L Levy <koden_kabau@yahoo.com>
2025-06-16 22:12:38 -0400

    Add battle data structures with serialization

--- pokemon/battle/__init__.py
+++ pokemon/battle/__init__.py
@@ -1,1 +1,15 @@
 from .damage import DamageResult, damage_calc
+from .battledata import BattleData, Team, Pokemon, TurnData, Field, Move
+from .turnorder import calculateTurnorder
+
+__all__ = [
+    "DamageResult",
+    "damage_calc",
+    "BattleData",
+    "Team",
+    "Pokemon",
+    "TurnData",
+    "Field",
+    "Move",
+    "calculateTurnorder",
+]
