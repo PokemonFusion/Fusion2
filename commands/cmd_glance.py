7af2f0c8e0e43abe3179b18e0da489645a8ba3a7
Matthew L Levy <koden_kabau@yahoo.com>
2025-06-26 23:54:18 -0400

    Add +glance command

---
+++ commands/cmd_glance.py
@@ -0,0 +1,39 @@
+from evennia import Command
+from evennia.utils.evtable import EvTable
+from evennia.utils import utils
+
+class CmdGlance(Command):
+    """Show a brief overview of online characters in the room."""
+
+    key = "+glance"
+    locks = "cmd:all()"
+    help_category = "General"
+
+    def func(self):
+        caller = self.caller
+        location = caller.location
+        if not location:
+            caller.msg("You have no location to glance at.")
+            return
+
+        chars = []
+        for obj in location.contents:
+            if not obj.is_typeclass("typeclasses.characters.Character", exact=False):
+                continue
+            if not obj.sessions.all():
+                continue
+            chars.append(obj)
+
+        if not chars:
+            caller.msg("No online characters here.")
+            return
+
+        table = EvTable("Name", "Gender", "Species", "Idle")
+        for char in chars:
+            gender = char.db.gender or "Unknown"
+            species = char.db.fusion_species or "Human"
+            idle = char.idle_time
+            idle_str = utils.time_format(idle, 1) if idle is not None else "0s"
+            table.add_row(char.key, gender, species, idle_str)
+
+        caller.msg(str(table))
