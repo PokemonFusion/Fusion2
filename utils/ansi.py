73d0fb9248bbeaad8d4797d9fad5706a19696668
Matthew L Levy <koden_kabau@yahoo.com>
2025-07-14 21:19:37 -0400

    Add local HexColors utility and expose in ansi wrapper

--- utils/ansi.py
+++ utils/ansi.py
@@ -1,5 +1,8 @@
 from evennia.utils import ansi as _ansi
 
+# Local copy of Evennia's HexColors utility for converting hex color tags
+from .hex_colors import HexColors
+
 
 def _make_color_func(code: str):
     """Return a simple colour-wrapper for `code`."""
@@ -18,3 +21,6 @@ for _name, _code in [
         setattr(_ansi, _name, _make_color_func(_code))
 
 ansi = _ansi
+
+# expose hex color converter similar to Evennia
+hex2truecolor = HexColors()
