2d986d727198ee684b121fd88fd28ad0141fa868
Matthew L Levy <koden_kabau@yahoo.com>
2025-07-15 05:21:06 -0400

    Show room type on list

--- roomeditor/templatetags/roomeditor_tags.py
+++ roomeditor/templatetags/roomeditor_tags.py
@@ -10,3 +10,13 @@ def is_builder(user):
     if check:
         return user.is_superuser or check('Builder') or check('Builders')
     return user.is_superuser
+
+
+@register.filter(name="class_name")
+def class_name(path_or_obj):
+    """Return the short class name from a typeclass path or object."""
+    if hasattr(path_or_obj, "typeclass_path"):
+        path_or_obj = path_or_obj.typeclass_path
+    if not isinstance(path_or_obj, str):
+        return ""
+    return path_or_obj.split(".")[-1]
