66b28f7c851bab5204b4116383ac24c9f6545314
Matthew L Levy <koden_kabau@yahoo.com>
2025-07-30 03:08:50 -0400

    Add battle event dispatcher and integrate

--- tests/test_event_dispatcher.py
+++ tests/test_event_dispatcher.py
@@ -10,12 +10,7 @@ sys.path.insert(0, ROOT)
 # Minimal pokemon.battle package stub
 pkg_battle = types.ModuleType("pokemon.battle")
 pkg_battle.__path__ = []
-utils_stub = types.ModuleType("pokemon.battle.utils")
-utils_stub.get_modified_stat = lambda p, s: getattr(p.base_stats, s, 0)
-utils_stub.apply_boost = lambda *a, **k: None
-pkg_battle.utils = utils_stub
 sys.modules["pokemon.battle"] = pkg_battle
-sys.modules["pokemon.battle.utils"] = utils_stub
 
 # Load entity dataclasses
 ent_path = os.path.join(ROOT, "pokemon", "dex", "entities.py")
@@ -26,7 +21,7 @@ ent_spec.loader.exec_module(ent_mod)
 Stats = ent_mod.Stats
 Ability = ent_mod.Ability
 
-# Minimal pokemon.dex package stub
+# Minimal pokemon.dex stub
 pokemon_dex = types.ModuleType("pokemon.dex")
 pokemon_dex.__path__ = []
 pokemon_dex.entities = ent_mod
@@ -35,13 +30,13 @@ pokemon_dex.Move = ent_mod.Move
 pokemon_dex.Pokemon = ent_mod.Pokemon
 sys.modules["pokemon.dex"] = pokemon_dex
 
-# Minimal pokemon.data stub used by damage_calc
+# Minimal pokemon.data stub
 data_stub = types.ModuleType("pokemon.data")
 data_stub.__path__ = []
 data_stub.TYPE_CHART = {}
 sys.modules["pokemon.data"] = data_stub
 
-# Load damage module and expose damage_calc
+# Load damage module
 damage_path = os.path.join(ROOT, "pokemon", "battle", "damage.py")
 d_spec = importlib.util.spec_from_file_location("pokemon.battle.damage", damage_path)
 d_mod = importlib.util.module_from_spec(d_spec)
@@ -72,50 +67,47 @@ ActionType = eng_mod.ActionType
 BattleType = eng_mod.BattleType
 
 
-def test_ability_callbacks_run():
-    records = {"start": 0, "switch": 0, "end": 0, "battles": []}
+def test_dispatcher_callbacks_fire():
+    events = []
+    battle_move = BattleMove("Tackle", power=40, accuracy=100)
 
-    def on_start(poke, battle):
-        records["start"] += 1
-        records["battles"].append(battle)
+    user = Pokemon("User")
+    user.base_stats = Stats(hp=100, atk=50, def_=50, spa=50, spd=50, spe=50)
+    user.num = 1
+    user.types = ["Normal"]
+    target = Pokemon("Target")
+    target.base_stats = Stats(hp=100, atk=50, def_=50, spa=50, spd=50, spe=50)
+    target.num = 2
+    target.types = ["Normal"]
 
-    def on_switch(poke, battle):
-        records["switch"] += 1
-
-    def on_end(poke, battle):
-        records["end"] += 1
-
-    ability = Ability(name="Dummy", num=0, raw={
-        "onStart": on_start,
-        "onSwitchIn": on_switch,
-        "onEnd": on_end,
-    })
-
-    user = Pokemon("User", ability=ability)
-    target = Pokemon("Target", ability=ability)
-    base = Stats(hp=100, atk=50, def_=50, spa=50, spd=50, spe=50)
-    for poke, num in ((user, 1), (target, 2)):
-        poke.base_stats = base
-        poke.num = num
-        poke.types = ["Normal"]
-
-    move = BattleMove("Tackle", power=40, accuracy=100)
     p1 = BattleParticipant("P1", [user], is_ai=False)
     p2 = BattleParticipant("P2", [target], is_ai=False)
     p1.active = [user]
     p2.active = [target]
-    action = Action(p1, ActionType.MOVE, p2, move, move.priority)
+    action = Action(p1, ActionType.MOVE, p2, battle_move, battle_move.priority)
     p1.pending_action = action
     battle = Battle(BattleType.WILD, [p1, p2])
+
+    battle.dispatcher.register("switch_in", lambda **_: events.append("switch_in"))
+    battle.dispatcher.register("before_move", lambda **_: events.append("before_move"))
+    battle.dispatcher.register("after_move", lambda **_: events.append("after_move"))
+    battle.dispatcher.register("end_turn", lambda **_: events.append("end_turn"))
+
     random.seed(0)
     battle.run_turn()
 
-    assert records["start"] == 2
-    assert records["switch"] == 2
-    assert records["end"] == 2
-    assert records["battles"].count(battle) == 2
+    assert events.count("switch_in") == 2
+    assert "before_move" in events
+    assert "after_move" in events
+    assert "end_turn" in events
 
 # Cleanup
-
-del sys.modules["pokemon.dex"]
-del sys.modules["pokemon.data"]
+for mod in [
+    "pokemon.dex",
+    "pokemon.data",
+    "pokemon.battle",
+    "pokemon.battle.engine",
+    "pokemon.battle.damage",
+    "pokemon.battle.battledata",
+]:
+    sys.modules.pop(mod, None)
