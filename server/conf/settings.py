848876d3b6f939ac23c287fcd2fe9bc4b86fbe0e
Koden02 <koden_kabau@yahoo.com>
2025-08-19 16:34:56 -0400

    Adjusted settings.

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -45,7 +45,7 @@ AUTO_PUPPET_ON_LOGIN = False
 # Settings given in secret_settings.py override those in this file.
 ######################################################################
 try:
-    from server.conf.secret_settings import *
+    from .secret_settings import *
 except ImportError:
     print("secret_settings.py file not found or failed to import.")
 
